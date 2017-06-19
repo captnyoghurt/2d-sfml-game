4052e78523fbc1fa68b26b6f18ec01a16ffaaa10
Yoghurt <paul.coignet@student.ecp.fr>
2017-06-19 11:21:27 +0200

    Ajout des prototypes des méthodes

--- ConsoleApplication1/Map/ManageRenderTexture.h
+++ ConsoleApplication1/Map/ManageRenderTexture.h
@@ -13,9 +13,24 @@ public:
 	~ManageRenderTexture();
 
 public:
+	// Return the RenderTexture where all the surfaces are printed
+	sf::RenderTexture getRenderTexture() const;
+	// Return the render texture surface
+	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator getRenderTextureSurface();
+	// Return the vector of all the surfaces on the render texture
+	std::vector<std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator> getSurfaces() const;
+	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator getTheSurface(int n);
+
+	// Return the RenderTexture where all the surfaces are printed with modifying possibilities
+	sf::RenderTexture& getRealRenderTexture();
+	// Return the render texture surface with modifying possibilities
+	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator& getRealRenderTextureSurface();
+	// Return the vector of all the surfaces on the render texture with modifying possibilities
+	std::vector<std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator>& getRealSurfaces();
 
 private:
 	sf::RenderTexture m_renderTexture;
+	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator m_renderTextureSurface;
 	std::vector<std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator>  m_surfaces;
 };
 
