1e3b02b0f307b9fe3d8f3821e2ed1fe70ec76396
Yoghurt <paul.coignet@student.ecp.fr>
2017-06-22 13:34:58 +0200

    Ajout du squelette des méthodes de MenuBattleSide

--- ConsoleApplication1/Menu/MenuBattleSide.cpp
+++ ConsoleApplication1/Menu/MenuBattleSide.cpp
@@ -10,3 +10,27 @@ MenuBattleSide::MenuBattleSide(ManageRessources& ress, ManageSurfaces& surf, int
 MenuBattleSide::~MenuBattleSide()
 {
 }
+
+
+// Load the menu
+int MenuBattleSide::load(ManageRessources& ress, ManageSurfaces& surf, const int &x, const int &y, const int &w, const int &h)
+{
+	// [TODO]
+	return 0;
+}
+
+
+// Update surfaces
+int MenuBattleSide::update(Game &g)
+{
+	// [TODO]
+	return 0;
+}
+
+
+// Close the menu
+int MenuBattleSide::close(ManageSurfaces& surf)
+{
+	// [TODO]
+	return 0;
+}
