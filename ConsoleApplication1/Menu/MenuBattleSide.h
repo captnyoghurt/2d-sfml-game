492f5d86cdb1dc270ebb9edee56b93b9737749ba
Yoghurt <paul.coignet@student.ecp.fr>
2017-06-22 13:26:38 +0200

    Création des prototypes des méthodes de MenuBattleSide

--- ConsoleApplication1/Menu/MenuBattleSide.h
+++ ConsoleApplication1/Menu/MenuBattleSide.h
@@ -1,16 +1,44 @@
 #pragma once
 
+#include "M_choice.h"
 #include "Menu.h"
+#include <vector>
 
 class MenuBattleSide : public Menu
 {
+public:
+	enum e_menuBattleSideItems{NAME, HP_TEXT, HP_IMAGE, MP_TEXT, MP_IMAGE, TP_TEXT, TP_IMAGE, TOTAL};
+
 public:
 	MenuBattleSide(ManageRessources& ress, ManageSurfaces& surf, int lastEventLayer);
 	~MenuBattleSide();
 
 public:
+	// Return the cursor for the right surface
+	std::list < std::pair < ManageSurfaces::e_thing, std::shared_ptr<Surface> > >::iterator getCursor();
+	// Return the current selected choice
+	int getSelectedChoice() const;
+	// Return the maximum number of choices
+	int getNumberMaxChoices() const;
 
+	// Return all the choices with modifying possibilities
+	std::vector< std::shared_ptr<M_choice> >& getChoices();
+
+	// Modify the current selected choice
+	int getSelectedChoice(const int &s);
+	// Modify the maximum number of choices
+	int getNumberMaxChoices(const int &c);
+
+	// Load the menu
+	virtual int load(ManageRessources& ress, ManageSurfaces& surf, const int &x, const int &y, const int &w = -1, const int &h = -1);
+	// Update surfaces
+	virtual int update(Game &g);
+	// Close the menu
+	virtual int close(ManageSurfaces& surf);
 private:
-	
+	std::vector < std::shared_ptr<M_choice> > m_choices;
+	std::list < std::pair < ManageSurfaces::e_thing, std::shared_ptr<Surface> > >::iterator m_cursorSurfaceRight;
+	int m_selectedChoice;
+	int m_numberMaxChoices;
 };
 
