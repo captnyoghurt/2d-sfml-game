b5467f2767bd5e7d8dcae6466baf1c5e4dff82c1
Yoghurt <paul.coignet@student.ecp.fr>
2017-07-07 21:37:45 +0200

    Add (MenuBattle.cpp) getter setter and getReal

--- ConsoleApplication1/Menu/MenuBattle.cpp
+++ ConsoleApplication1/Menu/MenuBattle.cpp
@@ -10,3 +10,29 @@ MenuBattle::MenuBattle(Battle *b)
 MenuBattle::~MenuBattle()
 {
 }
+
+
+// Return the current active menu
+int MenuBattle::getActiveMenu() const
+{
+	return m_activeMenu;
+}
+
+
+// Return the menus with modifying possibilities
+std::vector<Menu*>& MenuBattle::getRealMenus()
+{
+	return m_menus;
+}
+
+
+// Modify the current menu
+int MenuBattle::setActiveMenu(const int &am)
+{
+	if (!((unsigned)am < MenuBattle::BM_TOTAL))
+		return -1;
+
+	m_activeMenu = am;
+
+	return 0;
+}
