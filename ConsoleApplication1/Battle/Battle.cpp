34847a3d2688fba03552348cec525811cd12bfb1
Yoghurt <paul.coignet@student.ecp.fr>
2017-07-09 20:41:46 +0200

    Add (Battle class) main methods

--- ConsoleApplication1/Battle/Battle.cpp
+++ ConsoleApplication1/Battle/Battle.cpp
@@ -1,4 +1,6 @@
 #include "Battle.h"
+#include "../Map/ManageRessources.h"
+#include "../Map/ManageSurfaces.h"
 
 
 
@@ -9,4 +11,87 @@ Battle::Battle()
 
 Battle::~Battle()
 {
+}
+
+
+// Return if the battle has started
+bool Battle::getStarted() const
+{
+	return m_started;
+}
+
+
+// Return the enemie team
+Enemies Battle::getEnemies() const
+{
+	return m_enemieTeam;
+}
+
+
+// Return the allie team
+TeamBattle Battle::getAllies() const
+{
+	return m_alliesTeam;
+}
+
+
+// Return the enemie team with modifying possibilities
+Enemies& Battle::getRealEnemies()
+{
+	return m_enemieTeam;
+}
+
+
+// Return the allie team with modifiying possibilities
+TeamBattle& Battle::getRealAllies()
+{
+	return m_alliesTeam;
+}
+
+
+// Return the battleMenu with modifying possibilities
+BattleMenu& Battle::getRealBattleMenu()
+{
+	return *m_battleMenu;
+}
+
+
+// Return the battleEventManager with modifying possibilities
+BattleEventManager& Battle::getRealBattleEventManager()
+{
+	return m_battleEventManager;
+}
+
+
+// Return the background with modifying possibilities
+std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator& Battle::getRealBackground()
+{
+	return m_background;
+}
+
+
+// Start a battle according to the arguments
+int Battle::start(std::string backgroundFilename, TeamBattle team, std::string enemies, ManageRessources &ress, ManageSurfaces &surf, int lastEventLayer)
+{
+	// [TODO]
+
+	return 0;
+}
+
+
+// Update the battle
+int Battle::update(ManageRessources &ress, ManageSurfaces& surf)
+{
+	// [TODO]
+
+	return 0;
+}
+
+
+// End the battle
+int Battle::end()
+{
+	// [TODO]
+
+	return 0;
 }
--- ConsoleApplication1/Battle/Battle.h
+++ ConsoleApplication1/Battle/Battle.h
@@ -3,6 +3,11 @@
 #include "Enemies.h"
 #include "Fighter.h"
 #include "TeamBattle.h"
+#include "BattleEventManager.h"
+
+class BattleMenu;
+class ManageRessources;
+class ManageSurfaces;
 
 class Battle
 {
@@ -11,10 +16,37 @@ public:
 	~Battle();
 
 public:
+	// Return if the battle has started
+	bool getStarted() const;
+	// Return the enemie team
+	Enemies getEnemies() const;
+	// Return the allie team
+	TeamBattle getAllies() const;
+	
+	// Return the enemie team with modifying possibilities
+	Enemies& getRealEnemies();
+	// Return the allie team with modifiying possibilities
+	TeamBattle& getRealAllies();
+	// Return the battleMenu with modifying possibilities
+	BattleMenu& getRealBattleMenu();
+	// Return the battleEventManager with modifying possibilities
+	BattleEventManager& getRealBattleEventManager();
+	// Return the background with modifying possibilities
+	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator& getRealBackground();
+
+	// Start a battle according to the arguments
+	int start(std::string backgroundFilename, TeamBattle team, std::string enemies, ManageRessources &ress, ManageSurfaces &surf, int lastEventLayer);
+	// Update the battle
+	int update(ManageRessources &ress, ManageSurfaces& surf);
+	// End the battle
+	int end();
 
 private:
+	bool m_started;
 	Enemies m_enemieTeam;
 	TeamBattle m_alliesTeam;
+	BattleMenu *m_battleMenu;
+	BattleEventManager m_battleEventManager;
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator m_background;
 };
 
