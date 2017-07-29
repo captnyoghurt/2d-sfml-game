400f4ccd2bba32e6d096d8149ef336876e5fee73
Yoghurt <paul.coignet@student.ecp.fr>
2017-07-29 16:15:46 +0200

    Fix (MenuDialogBox) warning for the stream position

--- ConsoleApplication1/Menu/MenuDialogBox.cpp
+++ ConsoleApplication1/Menu/MenuDialogBox.cpp
@@ -78,15 +78,11 @@ int MenuDialogBox::setText(const std::string &text)
 // Add text to the stream
 int MenuDialogBox::addText(const std::string &text)
 {
-	//m_textStream << text.c_str();
 	m_text += text;
-	int pos = m_textStream.tellg();
 	
 	m_textStream.str("");
 	m_textStream.clear();
 	m_textStream << text;
-
-	//m_textStream.seekg(pos);
 
 	return 0;
 }
