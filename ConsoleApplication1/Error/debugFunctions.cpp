bc391757a1a7a8dc296d31123faeff6d321dbbc6
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-06 17:00:42 +0200

    Add (debugFunctions) files

---
+++ ConsoleApplication1/Error/debugFunctions.cpp
@@ -0,0 +1,23 @@
+#include "debugFunctions.h"
+
+#ifdef DEBUG_MODE_ON
+
+#include <iostream>
+
+int DEB_ALLOCATED_SURFACE(0);
+int DEB_ALLOCATED_TEXT(0);
+int DEB_ALLOCATED_SPRITE(0);
+
+
+void deb_start()
+{
+
+}
+
+
+void deb_end()
+{
+
+}
+
+#endif // DEBUG_MODE_ON
