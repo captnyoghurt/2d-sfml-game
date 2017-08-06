c16fa1146c7aa793666bc46b57d1cc5f7c797338
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-06 17:16:28 +0200

    Add (debugFunctions) implementation

--- ConsoleApplication1/Error/debugFunctions.cpp
+++ ConsoleApplication1/Error/debugFunctions.cpp
@@ -2,6 +2,8 @@
 
 #ifdef DEBUG_MODE_ON
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 
 int DEB_ALLOCATED_SURFACE(0);
@@ -11,13 +13,18 @@ int DEB_ALLOCATED_SPRITE(0);
 
 void deb_start()
 {
-
+	freopen("err.txt", "w", stderr);
 }
 
 
 void deb_end()
 {
-
+	std::cerr << "\n\n\n##############################################\n"
+		<< "###            END OF DEBUGING             ###\n"
+		<< "##############################################\n"
+		<< "\n\tLeak allocated surface :" << DEB_ALLOCATED_SURFACE
+		<< "\n\tLeak allocated text :" << DEB_ALLOCATED_TEXT
+		<< "\n\tLeak allocated sprite :" << DEB_ALLOCATED_SPRITE;
 }
 
 #endif // DEBUG_MODE_ON
