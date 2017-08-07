a382b3761d7da7c9ce06a4ea38ffc248bdaf5284
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-07 10:25:34 +0200

    Add (ManageHitbox) main methods

---
+++ ConsoleApplication1/Map/Physic/ManageHitbox.cpp
@@ -0,0 +1,64 @@
+#include "ManageHitbox.h"
+#include "../../Error/ValueException.h"
+
+
+
+ManageHitbox::ManageHitbox()
+{
+}
+
+
+ManageHitbox::~ManageHitbox()
+{
+	m_hitbox.clear();
+	m_tileHitbox.clear();
+}
+
+
+// Return the tile hitbox
+std::map< std::pair<int, int>, Hitbox > ManageHitbox::getTileHitbox() const
+{
+	return m_tileHitbox;
+}
+
+
+// Return the normal hitbox
+std::deque< Hitbox > ManageHitbox::getHitbox() const
+{
+	return m_hitbox;
+}
+
+
+// Return the tile hitbox with modifying possibilities
+std::map< std::pair<int, int>, Hitbox >& ManageHitbox::getRealTileHitbox()
+{
+	return m_tileHitbox;
+}
+
+
+// Return the normal hitbox with modifying possibilities
+std::deque< Hitbox >& ManageHitbox::getRealHitbox()
+{
+	return m_hitbox;
+}
+
+
+// Add a tile hitbox
+std::shared_ptr<Hitbox> ManageHitbox::addTileHitbox(const int &tx, const int &ty, const Hitbox& hb)
+{
+	if (tx < 0 || ty < 0)
+		THROW_VALUE("Wrong tile hitbox position : " + std::to_string(tx) + std::to_string(ty));
+
+	m_tileHitbox.insert(std::make_pair(std::make_pair(tx, ty), hb));
+
+	return std::make_shared<Hitbox>(m_tileHitbox.at(std::make_pair(tx, ty)));
+}
+
+
+// Add a hitbox
+std::shared_ptr<Hitbox> ManageHitbox::addHitbox(const Hitbox& hb, const int &id)
+{
+	m_hitbox.push_back(hb);
+
+	return std::make_shared<Hitbox>(m_hitbox.back());
+}
---
+++ ConsoleApplication1/Map/Physic/ManageHitbox.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <deque>
+#include "Hitbox.h"
+
+class ManageHitbox
+{
+public:
+	ManageHitbox();
+	~ManageHitbox();
+
+public:
+	// Return the tile hitbox
+	std::map< std::pair<int, int>, Hitbox > getTileHitbox() const;
+	// Return the normal hitbox
+	std::deque< Hitbox > getHitbox() const;
+
+	// Return the tile hitbox with modifying possibilities
+	std::map< std::pair<int, int>, Hitbox >& getRealTileHitbox();
+	// Return the normal hitbox with modifying possibilities
+	std::deque< Hitbox >& getRealHitbox();
+
+	// Add a tile hitbox
+	std::shared_ptr<Hitbox> addTileHitbox(const int &tx, const int &ty, const Hitbox& hb);
+	// Add a hitbox
+	std::shared_ptr<Hitbox> addHitbox(const Hitbox& hb, const int &id = -1);
+
+private:
+	std::map< std::pair<int, int>, Hitbox > m_tileHitbox;
+	std::deque< Hitbox > m_hitbox;
+};
+
