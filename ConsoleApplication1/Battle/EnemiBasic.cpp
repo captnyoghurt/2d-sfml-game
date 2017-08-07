4070f9d7724ddd2dc37a7bd2194bf7fb44b3bfc0
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-07 18:36:58 +0200

    Add (EnemiBasic) virtual chooseEvent method

--- ConsoleApplication1/Battle/EnemiBasic.cpp
+++ ConsoleApplication1/Battle/EnemiBasic.cpp
@@ -1,4 +1,7 @@
 #include "EnemiBasic.h"
+#include "B_Event.h"
+#include "B_EventAttack.h"
+#include "Battle.h"
 
 
 
@@ -9,4 +12,28 @@ EnemiBasic::EnemiBasic()
 
 EnemiBasic::~EnemiBasic()
 {
+}
+
+
+// Take a decision for the next battle action
+std::shared_ptr<B_Event> EnemiBasic::chooseEvent(Battle &b)
+{
+	std::uniform_int_distribution<int> distribution(1, b.getAllies().getNumberTeamMateAlive());
+	int n(distribution(m_generator));
+
+	for (unsigned int i(0); i < b.getAllies().getTeam().size(); i++)
+	{
+		if (!b.getAllies().getTeam().at(i).isDead())
+			n--;
+
+		if (n == 0)
+		{
+			B_EventAttack evt(this->getName() + " attaque !");
+			evt.getRealEnemyDestination().push_back(std::make_shared<TeamMate>(b.getRealAllies().getRealTeam().at(i)));
+
+			return std::make_shared<B_EventAttack>(evt);
+		}
+	}
+
+	return std::make_shared<B_Event>(B_Event());
 }
--- ConsoleApplication1/Battle/EnemiBasic.h
+++ ConsoleApplication1/Battle/EnemiBasic.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <random>
 #include "Fighter.h"
 
 class EnemiBasic : public Fighter
@@ -7,5 +8,12 @@ class EnemiBasic : public Fighter
 public:
 	EnemiBasic();
 	~EnemiBasic();
+
+public:
+	// Take a decision for the next battle action
+	virtual std::shared_ptr<B_Event> chooseEvent(Battle &b);
+
+private:
+	std::default_random_engine m_generator;
 };
 
