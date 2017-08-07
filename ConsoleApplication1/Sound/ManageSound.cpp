c5fe108eea6112f599058e7354b738c58de096ee
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-07 20:59:15 +0200

    Add (ManageSound) main attributs

--- ConsoleApplication1/Sound/ManageSound.cpp
+++ ConsoleApplication1/Sound/ManageSound.cpp
@@ -10,3 +10,31 @@ ManageSound::ManageSound()
 ManageSound::~ManageSound()
 {
 }
+
+
+// Return the list of sounds
+std::list<std::shared_ptr<sf::Sound>> ManageSound::getSounds() const
+{
+	return m_sounds;
+}
+
+
+// Return the stack of musics
+std::stack<std::shared_ptr<sf::Music>> ManageSound::getMusics() const
+{
+	return m_musics;
+}
+
+
+// Return the list of sounds with modifying possibilities
+std::list<std::shared_ptr<sf::Sound>>& ManageSound::getRealSounds()
+{
+	return m_sounds;
+}
+
+
+// Return the stack of musics with modifying possibilities
+std::stack<std::shared_ptr<sf::Music>>& ManageSound::getRealMusics()
+{
+	return m_musics;
+}
--- ConsoleApplication1/Sound/ManageSound.h
+++ ConsoleApplication1/Sound/ManageSound.h
@@ -1,8 +1,29 @@
 #pragma once
+
+#include <memory>
+#include <list>
+#include <stack>
+#include <SFML/Audio.hpp>
+
 class ManageSound
 {
 public:
 	ManageSound();
 	~ManageSound();
+
+public:
+	// Return the list of sounds
+	std::list<std::shared_ptr<sf::Sound>> getSounds() const;
+	// Return the stack of musics
+	std::stack<std::shared_ptr<sf::Music>> getMusics() const;
+
+	// Return the list of sounds with modifying possibilities
+	std::list<std::shared_ptr<sf::Sound>>& getRealSounds();
+	// Return the stack of musics with modifying possibilities
+	std::stack<std::shared_ptr<sf::Music>>& getRealMusics();
+
+public:
+	std::list<std::shared_ptr<sf::Sound>> m_sounds;
+	std::stack<std::shared_ptr<sf::Music>> m_musics;
 };
 
