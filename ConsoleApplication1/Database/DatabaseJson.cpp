183f93b9ffc0992ed86292cc43d0417a45093fd6
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-08 16:56:15 +0200

    Add (DatabaseJson) roots and readers attributes

--- ConsoleApplication1/Database/DatabaseJson.cpp
+++ ConsoleApplication1/Database/DatabaseJson.cpp
@@ -10,3 +10,17 @@ DatabaseJson::DatabaseJson()
 DatabaseJson::~DatabaseJson()
 {
 }
+
+
+// Return the roots with modifying possibilities
+std::vector<Json::Value>& DatabaseJson::getRealRoots()
+{
+	return m_roots;
+}
+
+
+// Return the readers with modifying possibilities
+std::vector<Json::Reader>& DatabaseJson::getRealReaders()
+{
+	return m_readers;
+}
--- ConsoleApplication1/Database/DatabaseJson.h
+++ ConsoleApplication1/Database/DatabaseJson.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include "json/json.h"
 
 class DatabaseJson
@@ -7,5 +8,15 @@ class DatabaseJson
 public:
 	DatabaseJson();
 	~DatabaseJson();
+
+public:
+	// Return the roots with modifying possibilities
+	std::vector<Json::Value>& getRealRoots();
+	// Return the readers with modifying possibilities
+	std::vector<Json::Reader>& getRealReaders();
+
+private:
+	std::vector<Json::Value> m_roots;
+	std::vector<Json::Reader> m_readers;
 };
 
