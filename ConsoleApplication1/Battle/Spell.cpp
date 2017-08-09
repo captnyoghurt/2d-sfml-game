abd2d1c72d93b816ccfd0090558c5fb1e4c5c1d0
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-09 12:20:28 +0200

    Add (Spell) id and name attribut

--- ConsoleApplication1/Battle/Spell.cpp
+++ ConsoleApplication1/Battle/Spell.cpp
@@ -1,4 +1,5 @@
 #include "Spell.h"
+#include "../Error/ValueException.h"
 
 
 
@@ -9,4 +10,39 @@ Spell::Spell()
 
 Spell::~Spell()
 {
+}
+
+
+// Return the id
+int Spell::getId() const
+{
+	return m_id;
+}
+
+
+// Return the name
+std::string Spell::getName() const
+{
+	return m_name;
+}
+
+
+// Modify the id
+int Spell::setId(const int &id)
+{
+	if (id <= 0)
+		THROW_VALUE(std::to_string(id));
+
+	m_id = id;
+
+	return id;
+}
+
+
+// Modify the name
+int Spell::setName(const std::string &name)
+{
+	m_name = name;
+
+	return 0;
 }
--- ConsoleApplication1/Battle/Spell.h
+++ ConsoleApplication1/Battle/Spell.h
@@ -1,8 +1,26 @@
 #pragma once
+
+#include "BattleEffects.h"
+
 class Spell
 {
 public:
 	Spell();
 	~Spell();
+
+public:
+	// Return the id
+	int getId() const;
+	// Return the name
+	std::string getName() const;
+
+	// Modify the id
+	int setId(const int &id);
+	// Modify the name
+	int setName(const std::string &name);
+
+public:
+	int m_id;
+	std::string m_name;
 };
 
