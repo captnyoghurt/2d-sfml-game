28110d9108ce9cbe795cf78d2380ab366768c862
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-10 13:31:24 +0200

    Add (MenuSpells) class

---
+++ ConsoleApplication1/Menu/MenuSpells.cpp
@@ -0,0 +1,12 @@
+#include "MenuSpells.h"
+
+
+
+MenuSpells::MenuSpells()
+{
+}
+
+
+MenuSpells::~MenuSpells()
+{
+}
---
+++ ConsoleApplication1/Menu/MenuSpells.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "MenuDialogBox.h"
+#include "MenuChoice.h"
+#include "../Battle/Spell.h"
+
+class MenuSpells
+{
+public:
+	MenuSpells();
+	~MenuSpells();
+};
+
