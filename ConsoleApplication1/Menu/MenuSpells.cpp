1e7da6379eaa3d44549da7519093fea14d1cad97
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-10 13:37:01 +0200

    Add (MenuSpells) submenus

--- ConsoleApplication1/Menu/MenuSpells.cpp
+++ ConsoleApplication1/Menu/MenuSpells.cpp
@@ -2,11 +2,27 @@
 
 
 
-MenuSpells::MenuSpells()
+MenuSpells::MenuSpells(ManageRessources& ress, ManageSurfaces& surf, int lastEventLayer) :
+	m_dialogBox(ress, surf, lastEventLayer),
+	m_choices(ress, surf, lastEventLayer)
 {
 }
 
 
 MenuSpells::~MenuSpells()
 {
+}
+
+
+// Return the dialog box with modifying possibilities
+MenuDialogBox& MenuSpells::getRealDialogBox()
+{
+	return m_dialogBox;
+}
+
+
+// Return the menu choice with modifying possibilities
+MenuChoice& MenuSpells::getRealMenuChoices()
+{
+	return m_choices;
 }
--- ConsoleApplication1/Menu/MenuSpells.h
+++ ConsoleApplication1/Menu/MenuSpells.h
@@ -7,7 +7,17 @@
 class MenuSpells
 {
 public:
-	MenuSpells();
+	MenuSpells(ManageRessources& ress, ManageSurfaces& surf, int lastEventLayer);
 	~MenuSpells();
+
+public:
+	// Return the dialog box with modifying possibilities
+	MenuDialogBox& getRealDialogBox();
+	// Return the menu choice with modifying possibilities
+	MenuChoice& getRealMenuChoices();
+
+public:
+	MenuDialogBox m_dialogBox;
+	MenuChoice m_choices;
 };
 
