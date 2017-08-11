57719231652f4fa5b1099913c5c844d13ff98ec2
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-11 11:32:03 +0200

    Add (Items) main attributes and getters setters

--- ConsoleApplication1/Items/Item.cpp
+++ ConsoleApplication1/Items/Item.cpp
@@ -1,4 +1,5 @@
 #include "Item.h"
+#include "../Error/ValueException.h"
 
 
 
@@ -9,4 +10,107 @@ Item::Item()
 
 Item::~Item()
 {
+}
+
+
+// Return the id
+int Item::getId() const
+{
+	return m_id;
+}
+
+
+// Return the name
+std::string Item::getName() const
+{
+	return m_name;
+}
+
+
+// Return the description
+std::string Item::getDescription() const
+{
+	return m_description;
+}
+
+
+// Return the price when bought
+int Item::getPriceBuy() const
+{
+	return m_priceBuy;
+}
+
+
+// Return the price when sold
+int Item::getPriceSell() const
+{
+	return m_priceSell;
+}
+
+
+// Return the effects
+std::deque<BattleEffects> Item::getEffects() const
+{
+	return m_effects;
+}
+
+
+// Return the effects with modifying possibilities
+std::deque<BattleEffects>& Item::getRealEffects()
+{
+	return m_effects;
+}
+
+
+// Modify the id
+int Item::setId(const int &id)
+{
+	if (id < 0)
+		THROW_VALUE("Impossible id : " + std::to_string(id));
+
+	m_id = id;
+
+	return 0;
+}
+
+
+// Modify the name
+int Item::setName(const std::string &name)
+{
+	m_name = name;
+
+	return 0;
+}
+
+
+// Modify the description
+int Item::setDescription(const std::string &description)
+{
+	m_description = description;
+
+	return 0;
+}
+
+
+// Modify the price when bought
+int Item::setPriceBuy(const int &p)
+{
+	if (p < 0)
+		THROW_VALUE("Impossible price : " + std::to_string(p));
+
+	m_priceBuy = p;
+
+	return 0;
+}
+
+
+// Modify the price when sold
+int Item::setPriceSell(const int &p)
+{
+	if (p < 0)
+		THROW_VALUE("Impossible price : " + std::to_string(p));
+
+	m_priceSell = p;
+
+	return 0;
 }
--- ConsoleApplication1/Items/Item.h
+++ ConsoleApplication1/Items/Item.h
@@ -1,8 +1,51 @@
 #pragma once
+
+#include <deque>
+#include <string>
+#include "../Battle/BattleEffects.h"
+
+
 class Item
 {
 public:
 	Item();
 	~Item();
+
+public:
+	// Return the id
+	int getId() const;
+	// Return the name
+	std::string getName() const;
+	// Return the description
+	std::string getDescription() const;
+	// Return the price when bought
+	int getPriceBuy() const;
+	// Return the price when sold
+	int getPriceSell() const;
+	// Return the effects
+	std::deque<BattleEffects> getEffects() const;
+
+	// Return the effects with modifying possibilities
+	std::deque<BattleEffects>& getRealEffects();
+
+	// Modify the id
+	int setId(const int &id);
+	// Modify the name
+	int setName(const std::string &name);
+	// Modify the description
+	int setDescription(const std::string &description);
+	// Modify the price when bought
+	int setPriceBuy(const int &p);
+	// Modify the price when sold
+	int setPriceSell(const int &p);
+	
+
+private:
+	int m_id;
+	std::string m_name;
+	std::string m_description;
+	int m_priceBuy;
+	int m_priceSell;
+	std::deque<BattleEffects> m_effects;
 };
 
