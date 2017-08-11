4e71ef7bc44677b30ea18e44a3b5825246b519e1
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-11 12:10:28 +0200

    Add (PieceOfEquipement) position attribut

--- ConsoleApplication1/Items/PieceOfEquipement.cpp
+++ ConsoleApplication1/Items/PieceOfEquipement.cpp
@@ -1,4 +1,5 @@
 #include "PieceOfEquipement.h"
+#include "../Error/ValueException.h"
 
 
 
@@ -9,4 +10,22 @@ PieceOfEquipement::PieceOfEquipement()
 
 PieceOfEquipement::~PieceOfEquipement()
 {
+}
+
+
+// Return the position of the piece of equipement
+PieceOfEquipement::e_POEPosition PieceOfEquipement::getPosition() const
+{
+	return m_position;
+}
+
+// Modify the position of the piece of equipement
+int PieceOfEquipement::setPosition(const PieceOfEquipement::e_POEPosition &pos)
+{
+	if (pos < 0 || pos > PieceOfEquipement::e_POEPosition::POE_TOTAL)
+		THROW_VALUE("Impossible value of equipement position : " + std::to_string(pos));
+
+	m_position = pos;
+
+	return 0;
 }
--- ConsoleApplication1/Items/PieceOfEquipement.h
+++ ConsoleApplication1/Items/PieceOfEquipement.h
@@ -6,7 +6,21 @@ class PieceOfEquipement :
 	public Item
 {
 public:
+	enum e_POEPosition {POE_SHOES = 0b00000001, POE_LEGS = 0b00000010, POE_BELT = 0b00000100, POE_CHEST = 0b00001000, POE_HAT = 0b00010000, 
+		POE_ACCESSORY = 0b00100000, POE_RIGHT_HAND = 0b01000000, POE_LEFT_HAND = 0b10000000, POE_TOTAL = 0b11111111};
+
+public:
 	PieceOfEquipement();
 	~PieceOfEquipement();
+
+public:
+	// Return the position of the piece of equipement
+	e_POEPosition getPosition() const;
+
+	// Modify the position of the piece of equipement
+	int setPosition(const e_POEPosition &pos);
+
+private:
+	e_POEPosition m_position;
 };
 
