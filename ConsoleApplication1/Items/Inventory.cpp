df79fb4d7316976b9acabb827f31d0370153ccd4
Yoghurt <paul.coignet@student.ecp.fr>
2017-08-12 14:02:49 +0200

    Add (Inventory) items attributes

--- ConsoleApplication1/Items/Inventory.cpp
+++ ConsoleApplication1/Items/Inventory.cpp
@@ -10,3 +10,45 @@ Inventory::Inventory()
 Inventory::~Inventory()
 {
 }
+
+
+// Return the items
+std::list<Item> Inventory::getItems() const
+{
+	return m_items;
+}
+
+
+// Return the orbs
+std::list<ItemOrb> Inventory::getOrbs() const
+{
+	return m_orbs;
+}
+
+
+// Return the POE
+std::list<PieceOfEquipement> Inventory::getPieceOfEquipement() const
+{
+	return m_pieceOfEquipement;
+}
+
+
+// Return the items with the modifying possibilities
+std::list<Item>& Inventory::getRealItems()
+{
+	return m_items;
+}
+
+
+// Return the orbs with the modifying possibilities
+std::list<ItemOrb>& Inventory::getRealOrbs()
+{
+	return m_orbs;
+}
+
+
+// Return the POE with the modifying possibilities
+std::list<PieceOfEquipement>& Inventory::getRealPieceOfEquipement()
+{
+	return m_pieceOfEquipement;
+}
--- ConsoleApplication1/Items/Inventory.h
+++ ConsoleApplication1/Items/Inventory.h
@@ -1,8 +1,34 @@
 #pragma once
+
+#include <list>
+#include "Item.h"
+#include "ItemOrb.h"
+#include "PieceOfEquipement.h"
+
 class Inventory
 {
 public:
 	Inventory();
 	~Inventory();
+
+public:
+	// Return the items
+	std::list<Item> getItems() const;
+	// Return the orbs
+	std::list<ItemOrb> getOrbs() const;
+	// Return the POE
+	std::list<PieceOfEquipement> getPieceOfEquipement() const;
+
+	// Return the items with the modifying possibilities
+	std::list<Item>& getRealItems();
+	// Return the orbs with the modifying possibilities
+	std::list<ItemOrb>& getRealOrbs();
+	// Return the POE with the modifying possibilities
+	std::list<PieceOfEquipement>& getRealPieceOfEquipement();
+
+private:
+	std::list<Item> m_items;
+	std::list<ItemOrb> m_orbs;
+	std::list<PieceOfEquipement> m_pieceOfEquipement;
 };
 
