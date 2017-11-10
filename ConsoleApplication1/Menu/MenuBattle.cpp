cc3c7b55aa86fae0166d04a56c2016177d805a5b
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 00:41:16 +0100

    Fix (MenuSpells) blocking when opening the menu

--- ConsoleApplication1/Menu/MenuBattle.cpp
+++ ConsoleApplication1/Menu/MenuBattle.cpp
@@ -191,7 +191,7 @@ int MenuBattle::af_MenuBattleEnter(Game &g)
 	else
 	{
 		ret = std::dynamic_pointer_cast<MenuChoice>(m_menus.at(m_activeMenu))->getChoices().at(std::dynamic_pointer_cast<MenuChoice>(m_menus.at(m_activeMenu))->getSelectedChoice())->getAction().getAction()(g);
-		if (ret == -1)
+		if (ret != 1)
 			m_isBlocking = true;
 		
 		// <debuging>
