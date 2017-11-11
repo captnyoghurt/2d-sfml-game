4dbb49415fe6e84d8e557c6b1e778709faca14ce
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 16:44:48 +0100

    Comment (ManageAnimation.h)

--- ConsoleApplication1/Animation/ManageAnimation.h
+++ ConsoleApplication1/Animation/ManageAnimation.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Manage all the animation to display.
+*
+*/
+
 #pragma once
 
 #include "Animation.h"
@@ -11,20 +25,35 @@ public:
 	~ManageAnimation();
 
 public:
-	// Return all the curretn animations
+	/**
+	* \brief Return all the curretn animations	.
+	*
+	* \return std::list<Animation> object.
+	*/
 	std::list<Animation> getAnimations() const;
 
-	// Return all the curretn animations with modification possibilities
+	/**
+	* \brief Return all the curretn animations with modification possibilities	.
+	*
+	* \return std::list<Animation>& object.
+	*/
 	std::list<Animation>& getRealAnimations();
 
-	// Add an animation
+	/**
+	* \brief Add an animation	.
+	*
+	* \return std::list<Animation>::iterator object.
+	*/
 	std::list<Animation>::iterator addAnimation(Animation a);
 
-	// Update all the animations
+	/**
+	* \brief Update all the animations	.
+	*
+	* \return int object.
+	*/
 	int update(Game &g);
 
 private:
 	std::list<Animation> m_animations;
 	sf::Clock m_clock;
 };
-
