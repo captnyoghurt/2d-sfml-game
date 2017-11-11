ce626b0f7fbd53a995635031f760f0c5bd5498d9
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:59:17 +0100

    Comment (BattleEventManager.h)

--- ConsoleApplication1/Battle/BattleEventManager.h
+++ ConsoleApplication1/Battle/BattleEventManager.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Execute the different event of a fight.
+*
+*/
+
 #pragma once
 
 #include <list>
@@ -14,38 +28,92 @@ public:
 	~BattleEventManager();
 
 public:
-	// Return the list of battle events
+	/**
+	* \brief Return the list of battle events	.
+	*
+	* \return std::list<std::shared_ptr<B_Event>> object.
+	*/
 	std::list<std::shared_ptr<B_Event>> getBattleEvents() const;
-	// Return if the battle event manager is waiting
+	/**
+	* \brief Return if the battle event manager is waiting	.
+	*
+	* \return bool object.
+	*/
 	bool getWaiting() const;
-	// Return if the execute has started
+	/**
+	* \brief Return if the execute has started	.
+	*
+	* \return bool object.
+	*/
 	bool getExecuteStarted() const;
 
-	// Return the list of battle events with modifying possibilities
+	/**
+	* \brief Return the list of battle events with modifying possibilities	.
+	*
+	* \return std::list<std::shared_ptr<B_Event>>& object.
+	*/
 	std::list<std::shared_ptr<B_Event>>& getRealBattleEvents();
 
-	// Modify if the battle event manager is waiting
+	/**
+	* \brief Modify if the battle event manager is waiting	.
+	*
+	* \param b The new value.
+	* \return int object.
+	*/
 	int setWaiting(const bool& b);
-	// Modify if the execute has started
+	/**
+	* \brief Modify if the execute has started	.
+	*
+	* \param b The new value.
+	* \return int object.
+	*/
 	int setExecuteStarted(const bool &b);
 
-	// Add an event to the top of the list
+	/**
+	* \brief Add an event to the top of the list	.
+	*
+	* \return int object.
+	*/
 	int addToFront(std::shared_ptr<B_Event> b);
-	// Create an event
+	/**
+	* \brief Create an event	.
+	*
+	* \return int object.
+	*/
 	int createEvent(std::shared_ptr<Fighter> source, std::shared_ptr<B_Event> type, Game &g);
-	// Add a destination for the event in construction
+	/**
+	* \brief Add a destination for the event in construction	.
+	*
+	* \return int object.
+	*/
 	int addDestinationForEvent(std::shared_ptr<Fighter> f, Game &g);
 
-	// Return if the manager is creating an event
+	/**
+	* \brief Return if the manager is creating an event	.
+	*
+	* \return bool object.
+	*/
 	bool isCreating() const;
-	// Clear the entire list
+	/**
+	* \brief Clear the entire list	.
+	*
+	* \return int object.
+	*/
 	int clear();
 
-	// Execute the header event
+	/**
+	* \brief Execute the header event	.
+	*
+	* \return int object.
+	*/
 	int execute(Game &g);
 
 protected:
-	// Tell the battle menu to ask a destination
+	/**
+	* \brief Tell the battle menu to ask a destination	.
+	*
+	* \return int object.
+	*/
 	int askDestination(Game &g);
 
 private:
@@ -54,4 +122,3 @@ private:
 	std::list<std::shared_ptr<B_Event> > m_battleEvents;
 	std::shared_ptr<B_Event> m_eventInConstruction;
 };
-
