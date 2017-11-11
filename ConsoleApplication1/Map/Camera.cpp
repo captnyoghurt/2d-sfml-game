59a8f165ff065af3858c248b601e6e461c6eec08
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:14:18 +0100

    Comment (Camera.cpp)

--- ConsoleApplication1/Map/Camera.cpp
+++ ConsoleApplication1/Map/Camera.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Camera of the map.
+*
+*/
+
 #include <iostream>
 #include "Camera.h"
 
