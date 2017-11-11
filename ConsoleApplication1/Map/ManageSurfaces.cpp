f77e25c19d4354fa1726d42ec6a10fa76c1375d8
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:15:23 +0100

    Comment (ManageSurfaces.cpp)

--- ConsoleApplication1/Map/ManageSurfaces.cpp
+++ ConsoleApplication1/Map/ManageSurfaces.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Save and manage the different surfaces.
+*
+*/
+
 #include <iostream>
 #include "ManageSurfaces.h"
 #include "../Error/ValueException.h"
