419b783b7062c7bca074cdbaf08da8389cc85da3
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:02:37 +0100

    Comment (Health.cpp)

--- ConsoleApplication1/Battle/Status/Health.cpp
+++ ConsoleApplication1/Battle/Status/Health.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Health of a fighter.
+*
+*/
+
 #include "../../constants.h"
 #include "Health.h"
 #include "Spell.h"
