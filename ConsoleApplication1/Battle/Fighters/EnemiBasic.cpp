58c2d8b099a47823fbb7c2116908b82f168fcbdd
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 16:50:03 +0100

    Comment (EnemiBasic.cpp)

--- ConsoleApplication1/Battle/Fighters/EnemiBasic.cpp
+++ ConsoleApplication1/Battle/Fighters/EnemiBasic.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  A simple enemi to fight.
+*
+*/
+
 #include "EnemiBasic.h"
 #include "../B_Event/B_Event.h"
 #include "../B_Event/B_EventAttack.h"
