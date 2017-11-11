7b64a24e555ff6f8d60d768a0a9ce742467cc112
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:15:32 +0100

    Comment (Map.cpp)

--- ConsoleApplication1/Map/Map.cpp
+++ ConsoleApplication1/Map/Map.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  The map information.
+*
+*/
+
 #include <memory>
 #include "../constants.h"
 #include "../Game.h"
