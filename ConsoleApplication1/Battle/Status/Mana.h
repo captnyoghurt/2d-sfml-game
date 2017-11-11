ecb5415a19b58019690d6d0962d08e99091fd4b6
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:03:12 +0100

    Comment (Mana.h)

--- ConsoleApplication1/Battle/Status/Mana.h
+++ ConsoleApplication1/Battle/Status/Mana.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Mana of a fighter.
+*
+*/
+
 #pragma once
 
 #include <list>
@@ -15,32 +29,73 @@ public:
 
 	Mana& operator=(const Mana& mana);
 public:
-	// Return the number of mana points
+	/**
+	* \brief Return the number of mana points	.
+	*
+	* \return int object.
+	*/
 	int getPoints() const;
-	// Return the maximum number of mana points
+	/**
+	* \brief Return the maximum number of mana points	.
+	*
+	* \return int object.
+	*/
 	int getMaxPoints() const;
-	// Return all the actual effects
+	/**
+	* \brief Return all the actual effects	.
+	*
+	* \return std::list<EffectOnCharacteristic> object.
+	*/
 	std::list<EffectOnCharacteristic> getEffects() const;
-	// Return all the actual adding effects
+	/**
+	* \brief Return all the actual adding effects	.
+	*
+	* \return std::list<EffectOnCharacteristic> object.
+	*/
 	std::list<EffectOnCharacteristic> getAddingEffects() const;
 
-	// Modify the number of mana points
+	/**
+	* \brief Modify the number of mana points	.
+	*
+	* \param p The new value.
+	* \return int object.
+	*/
 	int setPoints(const int &p);
-	// Modify the maximum number of mana
+	/**
+	* \brief Modify the maximum number of mana	.
+	*
+	* \param maxp The new value.
+	* \return int object.
+	*/
 	int setMaxPoints(const int &maxp);
 
-	// Say if a spell can be used
+	/**
+	* \brief Say if a spell can be used	.
+	*
+	* \return bool object.
+	*/
 	bool canCastSpell(const Spell &sp);
-	// Say if there is enought mana
+	/**
+	* \brief Say if there is enought mana	.
+	*
+	* \return bool object.
+	*/
 	bool enoughFor(const int &points);
 
-	// Use some mana points
+	/**
+	* \brief Use some mana points	.
+	*
+	* \return int object.
+	*/
 	int use(const int &points);
 	int use(const Spell &sp);
-	// Get some mana points
+	/**
+	* \brief Get some mana points	.
+	*
+	* \return int object.
+	*/
 	int recharge(const int &points);
 private:
 	int m_maxPoints;
 	int m_points;
 };
-
