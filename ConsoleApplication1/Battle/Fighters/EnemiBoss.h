eb3dee4401f89947b997839e9226865c755cf139
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 16:50:54 +0100

    Comment (EnemiBoss.h)

--- ConsoleApplication1/Battle/Fighters/EnemiBoss.h
+++ ConsoleApplication1/Battle/Fighters/EnemiBoss.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  A big scripted enemi.
+*
+*/
+
 #pragma once
 
 #include "Fighter.h"
@@ -8,4 +22,3 @@ public:
 	EnemiBoss();
 	~EnemiBoss();
 };
-
