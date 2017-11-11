1970b06f67165638b0855fe1f30eb6abb8b6197c
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:13:50 +0100

    Comment (Hitbox.cpp)

--- ConsoleApplication1/Map/Physic/Hitbox.cpp
+++ ConsoleApplication1/Map/Physic/Hitbox.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  A physic hitbox, to see if there is a collision.
+*
+*/
+
 #include "Hitbox.h"
 #include "../../Error/ValueException.h"
 
