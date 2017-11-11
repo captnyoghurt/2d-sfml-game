0db928f4e7a27971822735c09731a9d156b5548f
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:01:35 +0100

    Comment (Battle.h)

--- ConsoleApplication1/Battle/Battle.h
+++ ConsoleApplication1/Battle/Battle.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Describe the battle and execute different turn.
+*
+*/
+
 #pragma once
 
 #include "Fighters/Enemies.h"
@@ -20,66 +34,172 @@ public:
 	~Battle();
 
 public:
-	// Return if the battle has started
+	/**
+	* \brief Return if the battle has started	.
+	*
+	* \return bool object.
+	*/
 	bool getStarted() const;
-	// Return if the turn has begun
+	/**
+	* \brief Return if the turn has begun	.
+	*
+	* \return bool object.
+	*/
 	bool getInTurn() const;
-	// Return if the choices for the turn has been made
+	/**
+	* \brief Return if the choices for the turn has been made	.
+	*
+	* \return bool object.
+	*/
 	bool getChoicesFinished() const;
-	// Return if the battle is shown
+	/**
+	* \brief Return if the battle is shown	.
+	*
+	* \return bool object.
+	*/
 	bool getShown() const;
  	// Return the number of turn since the begining of the battle
 	int getBattleTurn() const;
-	// Return the enemie team
+	/**
+	* \brief Return the enemie team	.
+	*
+	* \return Enemies object.
+	*/
 	Enemies getEnemies() const;
-	// Return the allie team
+	/**
+	* \brief Return the allie team	.
+	*
+	* \return TeamBattle object.
+	*/
 	TeamBattle getAllies() const;
-	// Return the selected TeamMate
+	/**
+	* \brief Return the selected TeamMate	.
+	*
+	* \return TeamMate object.
+	*/
 	TeamMate getSelectedTeamMate() const;
-	// Return the last key event layer
+	/**
+	* \brief Return the last key event layer	.
+	*
+	* \return int object.
+	*/
 	int getLastKeyEventLayer() const;
-	// Return the number of battle event created
+	/**
+	* \brief Return the number of battle event created	.
+	*
+	* \return int object.
+	*/
 	int getBattleEventCreated() const;
-	// Return the battle exit
+	/**
+	* \brief Return the battle exit	.
+	*
+	* \return e_BattleExit object.
+	*/
 	e_BattleExit getBattleExit() const;
 
-	// Return the enemie team with modifying possibilities
+	/**
+	* \brief Return the enemie team with modifying possibilities	.
+	*
+	* \return Enemies& object.
+	*/
 	Enemies& getRealEnemies();
-	// Return the allie team with modifiying possibilities
+	/**
+	* \brief Return the allie team with modifiying possibilities	.
+	*
+	* \return TeamBattle& object.
+	*/
 	TeamBattle& getRealAllies();
-	// Return the battleMenu with modifying possibilities
+	/**
+	* \brief Return the battleMenu with modifying possibilities	.
+	*
+	* \return MenuBattle& object.
+	*/
 	MenuBattle& getRealBattleMenu();
-	// Return the battleEventManager with modifying possibilities
+	/**
+	* \brief Return the battleEventManager with modifying possibilities	.
+	*
+	* \return BattleEventManager& object.
+	*/
 	BattleEventManager& getRealBattleEventManager();
-	// Return the background with modifying possibilities
+	/**
+	* \brief Return the background with modifying possibilities	.
+	*
+	* \return std::list<std::pair<ManageSurfaces::e_thing, object.
+	*/
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator& getRealBackground();
-	// Return the real battle order
+	/**
+	* \brief Return the real battle order	.
+	*
+	* \return std::vector< object.
+	*/
 	std::vector< std::shared_ptr<Fighter> >& getRealBattleOrder();
 
-	// Modify the battle exit
+	/**
+	* \brief Modify the battle exit	.
+	*
+	* \param be The new value.
+	* \return int object.
+	*/
 	int setBattleExit(const Battle::e_BattleExit &be);
-	// Modify if the battle is shown
+	/**
+	* \brief Modify if the battle is shown	.
+	*
+	* \param b The new value.
+	* \return int object.
+	*/
 	int setShown(const bool &b);
 
-	// Tell the battle it's updated
+	/**
+	* \brief Tell the battle it's updated	.
+	*
+	* \return int object.
+	*/
 	int gotUpdated();
 	
-	// Start a new battle turn
+	/**
+	* \brief Start a new battle turn	.
+	*
+	* \return int object.
+	*/
 	int startTurn(Game *g);
 
-	// Start a battle according to the arguments
+	/**
+	* \brief Start a battle according to the arguments	.
+	*
+	* \return int object.
+	*/
 	int start(const std::string &backgroundFilename, TeamBattle team, std::string enemies, Game *g);
-	// Update the battle
+	/**
+	* \brief Update the battle	.
+	*
+	* \return int object.
+	*/
 	int update(Game *g);
-	// End the battle
+	/**
+	* \brief End the battle	.
+	*
+	* \return int object.
+	*/
 	int end(Game *g);
 
 protected:
-	// Set m_battleOrder to make the right order
+	/**
+	* \brief Set m_battleOrder to make the right order	.
+	*
+	* \return int object.
+	*/
 	int definedOrder(Game *g);
-	// Choose battle event for the next player
+	/**
+	* \brief Choose battle event for the next player	.
+	*
+	* \return int object.
+	*/
 	int chooseBattleEvent(Game *g);
-	// Clear the battle attribute from last battle
+	/**
+	* \brief Clear the battle attribute from last battle	.
+	*
+	* \return int object.
+	*/
 	int clear();
 
 private:
@@ -90,14 +210,4 @@ private:
 	bool m_shown;
 	int m_battleTurn;
 	int m_lastKeyEventLayer;
-	int m_battleEventCreated;
-	e_BattleExit m_battleExit;
-	Enemies m_enemieTeam;
-	TeamBattle *m_alliesTeam;
-	std::shared_ptr<MenuBattle> m_battleMenu;
-	BattleEventManager m_battleEventManager;
-	std::list<sf::Texture>::iterator m_texture;
-	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator m_background;
-	std::vector< std::shared_ptr<Fighter> > m_battleOrder;
-};
-
+	in
