411f65da165de77611deddbcd0a57ce5f4f69350
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:01:59 +0100

    Comment (BattleEventManager.cpp)

--- ConsoleApplication1/Battle/BattleEventManager.cpp
+++ ConsoleApplication1/Battle/BattleEventManager.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Execute the different event of a fight.
+*
+*/
+
 #include <iostream>
 #include <memory>
 #include "BattleEventManager.h"
