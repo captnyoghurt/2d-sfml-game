774ee2213bcd9165579a01145358cfe5ee90c89a
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:06:40 +0100

    Comment (IG_KeyEvent.cpp)

--- ConsoleApplication1/Event/IG_KeyEvent.cpp
+++ ConsoleApplication1/Event/IG_KeyEvent.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Can trigger an IG_Action.
+*
+*/
+
 #include "IG_KeyEvent.h"
 #include "../Error/ValueException.h"
 
