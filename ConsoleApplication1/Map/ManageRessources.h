d996ff53503df90be4814599f881d2d594f3a213
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 18:01:41 +0100

    Comment (ManageRessources)

--- ConsoleApplication1/Map/ManageRessources.h
+++ ConsoleApplication1/Map/ManageRessources.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Save and manage the different ressources.
+*
+*/
+
 #pragma once
 
 #include <SFML\Graphics.hpp>
@@ -11,56 +25,140 @@ public:
 	~ManageRessources();
 
 public:
-	// Return all the fonts
+	/**
+	* \brief Return all the fonts	.
+	*
+	* \return std::list<sf::Font> object.
+	*/
 	std::list<sf::Font> getFonts() const;
-	// Return all the images
+	/**
+	* \brief Return all the images	.
+	*
+	* \return std::list<sf::Image> object.
+	*/
 	std::list<sf::Image> getImages() const;
-	// Return all the textures
+	/**
+	* \brief Return all the textures	.
+	*
+	* \return std::list<sf::Texture> object.
+	*/
 	std::list<sf::Texture> getTextures() const;
-	// Return all the sound buffers
+	/**
+	* \brief Return all the sound buffers	.
+	*
+	* \return std::list<sf::SoundBuffer> object.
+	*/
 	std::list<sf::SoundBuffer> getSoundBuffers() const;
 
-	// Return a font
+	/**
+	* \brief Return a font	.
+	*
+	* \return sf::Font& object.
+	*/
 	sf::Font& getTheFont(int n);
-	// Return a image
+	/**
+	* \brief Return a image	.
+	*
+	* \return sf::Image& object.
+	*/
 	sf::Image& getTheImage(int n);
-	// Return a texture
+	/**
+	* \brief Return a texture	.
+	*
+	* \return sf::Texture& object.
+	*/
 	sf::Texture& getTheTexture(int n);
-	// Return a sound buffer
+	/**
+	* \brief Return a sound buffer	.
+	*
+	* \return sf::SoundBuffer& object.
+	*/
 	sf::SoundBuffer& getTheSoundBuffer(int n);
 
-	// Return all the fonts with modifying possibilities
+	/**
+	* \brief Return all the fonts with modifying possibilities	.
+	*
+	* \return std::list<sf::Font>& object.
+	*/
 	std::list<sf::Font>& getRealFonts();
-	// Return all the images with modifying possibilities
+	/**
+	* \brief Return all the images with modifying possibilities	.
+	*
+	* \return std::list<sf::Image>& object.
+	*/
 	std::list<sf::Image>& getRealImages();
-	// Return all the textures with modifying possibilities
+	/**
+	* \brief Return all the textures with modifying possibilities	.
+	*
+	* \return std::list<sf::Texture>& object.
+	*/
 	std::list<sf::Texture>& getRealTextures();
-	// Return all the sound buffers with modifying possibilities
+	/**
+	* \brief Return all the sound buffers with modifying possibilities	.
+	*
+	* \return std::list<sf::SoundBuffer>& object.
+	*/
 	std::list<sf::SoundBuffer>& getRealSoundBuffers();
 
-	// Add a font ressource
+	/**
+	* \brief Add a font ressource	.
+	*
+	* \return std::list<sf::Font>::iterator object.
+	*/
 	std::list<sf::Font>::iterator addFont(sf::Font f);
 	std::list<sf::Font>::iterator addFont();
-	// Add an image ressource
+	/**
+	* \brief Add an image ressource	.
+	*
+	* \return std::list<sf::Image>::iterator object.
+	*/
 	std::list<sf::Image>::iterator addImage(sf::Image img);
 	std::list<sf::Image>::iterator addImage();
-	// Add a texture ressource
+	/**
+	* \brief Add a texture ressource	.
+	*
+	* \return std::list<sf::Texture>::iterator object.
+	*/
 	std::list<sf::Texture>::iterator addTexture(sf::Texture texture);
 	std::list<sf::Texture>::iterator addTexture();
-	// Add a sound buffer
+	/**
+	* \brief Add a sound buffer	.
+	*
+	* \return std::list<sf::SoundBuffer>::iterator object.
+	*/
 	std::list<sf::SoundBuffer>::iterator addSoundBuffer(sf::SoundBuffer sbuffer);
 	std::list<sf::SoundBuffer>::iterator addSoundBuffer();
 
-	// Delete a font ressource
+	/**
+	* \brief Delete a font ressource	.
+	*
+	* \return int object.
+	*/
 	int deleteFont(std::list<sf::Font>::iterator it);
-	// Delete an image ressource
+	/**
+	* \brief Delete an image ressource	.
+	*
+	* \return int object.
+	*/
 	int deleteImage(std::list<sf::Image>::iterator it);
-	// Delete a texture ressource
+	/**
+	* \brief Delete a texture ressource	.
+	*
+	* \return int object.
+	*/
 	int deleteTexture(std::list<sf::Texture>::iterator it);
-	// Delete a sound buffer
+	/**
+	* \brief Delete a sound buffer	.
+	*
+	* \return int object.
+	*/
 	int deleteSoundBuffer(std::list<sf::SoundBuffer>::iterator it);
 
-	// Clear all the ressources
+	/**
+	* \brief Clear all the ressources	.
+	*
+	* \return int object.
+	*/
 	int clear();
 private:
 	std::list<sf::Font> m_fonts;
@@ -68,4 +166,3 @@ private:
 	std::list<sf::Texture> m_textures;
 	std::list<sf::SoundBuffer> m_soundBuffers;
 };
-
