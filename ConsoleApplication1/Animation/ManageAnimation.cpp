7396e857fb044a965dce613a4ee69adbc7562331
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 16:44:34 +0100

    Comment (ManageAnimation.cpp)

--- ConsoleApplication1/Animation/ManageAnimation.cpp
+++ ConsoleApplication1/Animation/ManageAnimation.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Manage all the animation to display.
+*
+*/
+
 #include "../constants.h"
 #include "../Game.h"
 #include "ManageAnimation.h"
