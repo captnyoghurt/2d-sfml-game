19b7a06e8f53140bd86b8f7c74ab16ad74ccc12a
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:22:08 +0100

    Comment (Team.h)

--- ConsoleApplication1/Team/Team.h
+++ ConsoleApplication1/Team/Team.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  The team on the map.
+*
+*/
+
 #pragma once
 
 #include <vector>
@@ -18,106 +32,170 @@ public:
 	~Team();
 
 public:
-	// Return if it is moving
+	/**
+	* \brief Return if it is moving	.
+	*
+	* \return bool object.
+	*/
 	bool getMoving() const;
-	// Return if the camera should follow
+	/**
+	* \brief Return if the camera should follow	.
+	*
+	* \return bool object.
+	*/
 	bool getCameraFollowing() const;
-	// Return the abciss of the team
+	/**
+	* \brief Return the abciss of the team	.
+	*
+	* \return unsigned object.
+	*/
 	unsigned short getX() const;
-	// Return the y position of the team
+	/**
+	* \brief Return the y position of the team	.
+	*
+	* \return unsigned object.
+	*/
 	unsigned short getY() const;
-	// Return the width of the team
+	/**
+	* \brief Return the width of the team	.
+	*
+	* \return short object.
+	*/
 	short getWidth() const;
-	// Return the height of the team
+	/**
+	* \brief Return the height of the team	.
+	*
+	* \return short object.
+	*/
 	short getHeight() const;
-	// Return the mode
+	/**
+	* \brief Return the mode	.
+	*
+	* \return e_teamMode object.
+	*/
 	e_teamMode getMode() const;
-	// Return the current direction
+	/**
+	* \brief Return the current direction	.
+	*
+	* \return e_teamDirection object.
+	*/
 	e_teamDirection getDirection() const;
-	// Return the team
+	/**
+	* \brief Return the team	.
+	*
+	* \return std::vector<Character> object.
+	*/
 	std::vector<Character> getTeam() const;
-	// Return animations
+	/**
+	* \brief Return animations	.
+	*
+	* \return std::vector<std::list<Animation>::iterator> object.
+	*/
 	std::vector<std::list<Animation>::iterator> getAnimations() const;
-	// Return the walk stand surface
+	/**
+	* \brief Return the walk stand surface	.
+	*
+	* \return std::list<std::pair<ManageSurfaces::e_thing, object.
+	*/
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator getWalkStand() const;
-	// Return the hitbox
+	/**
+	* \brief Return the hitbox	.
+	*
+	* \return Hitbox object.
+	*/
 	Hitbox getHitbox() const;
-	// Return the inventory
+	/**
+	* \brief Return the inventory	.
+	*
+	* \return Inventory object.
+	*/
 	Inventory getInventory() const;
-	// Return the maxX of the team
+	/**
+	* \brief Return the maxX of the team	.
+	*
+	* \return short object.
+	*/
 	short getMaxX() const;
-	// Return the maxY of the team
+	/**
+	* \brief Return the maxY of the team	.
+	*
+	* \return short object.
+	*/
 	short getMaxY() const;
-	// Return the team battle
+	/**
+	* \brief Return the team battle	.
+	*
+	* \return TeamBattle object.
+	*/
 	TeamBattle getTeamBattle() const;
 
-	// Return the team with modifying possibilities
+	/**
+	* \brief Return the team with modifying possibilities	.
+	*
+	* \return std::vector<Character>& object.
+	*/
 	std::vector<Character>& getRealTeam();
-	// Return animations with modifying possibilities
+	/**
+	* \brief Return animations with modifying possibilities	.
+	*
+	* \return std::vector<std::list<Animation>::iterator>& object.
+	*/
 	std::vector<std::list<Animation>::iterator>& getRealAnimations();
-	// Return the walk stand surface with modifying possibilities
+	/**
+	* \brief Return the walk stand surface with modifying possibilities	.
+	*
+	* \return std::list<std::pair<ManageSurfaces::e_thing, object.
+	*/
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator& getRealWalkStand();
-	// Return the hitbox with modifying possibilities
+	/**
+	* \brief Return the hitbox with modifying possibilities	.
+	*
+	* \return Hitbox& object.
+	*/
 	Hitbox& getRealHitbox();
-	// Return the inventory with modifying possibilities
+	/**
+	* \brief Return the inventory with modifying possibilities	.
+	*
+	* \return Inventory& object.
+	*/
 	Inventory& getRealInventory();
-	// Return the team battle with modifying possibilities
+	/**
+	* \brief Return the team battle with modifying possibilities	.
+	*
+	* \return TeamBattle& object.
+	*/
 	TeamBattle& getRealTeamBattle();
 
-	// Modify if it is moving
+	/**
+	* \brief Modify if it is moving	.
+	*
+	* \param b The new value.
+	* \return int object.
+	*/
 	int setMoving(const bool &b);
-	// Modify if the camera should follow
+	/**
+	* \brief Modify if the camera should follow	.
+	*
+	* \param b The new value.
+	* \return int object.
+	*/
 	int setCameraFollowing(const bool &b);
-	// Modify the abciss of the team
+	/**
+	* \brief Modify the abciss of the team	.
+	*
+	* \param x The new value.
+	* \return int object.
+	*/
 	int setX(const unsigned short &x);
-	// Modify the y position of the team
+	/**
+	* \brief Modify the y position of the team	.
+	*
+	* \param y The new value.
+	* \return int object.
+	*/
 	int setY(const unsigned short &y);
-	// Modify the width of the team
-	int setWidth(const short &w);
-	// Modify the height of the team
-	int setHeight(const short &h);
-	// Modifying mode
-	int setMode(e_teamMode m);
-	// Modify the current direction
-	int setDirection(e_teamDirection dir);
-	// Modify all the dimensions
-	int setDimension(const unsigned short &x, const unsigned short &y, const short &w, const short &h);
-	// Modify the maxX of the team
-	int setMaxX(short mapWidth);
-	// Modify the maxY of the team
-	int setMaxY(short mapHeight);
-	// Modify the walk stand surface
-	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator setWalkStand(std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator l);
-
-	// Move the team
-	int move(const short &dx, const short &dy, e_teamDirection direction = UNKNOWN);
-	int stopMoving(Team::e_teamDirection dir = UNKNOWN);
-	//int moveWithCamera(const short &dx, const short &dy, Camera &c, e_teamDirection direction = UNKNOWN);
-	// Update the movement
-	int update(Camera &c, ManageHitbox &hm);
-	// Load the TeamBattle
-	int loadTeamBattle(Game &g);
-
-private:
-	std::vector<Character> m_team;
-	TeamBattle m_teamBattle;
-	e_teamMode m_mode;
-	e_teamDirection m_direction;
-	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator m_walkStand;
-	std::vector<std::list<Animation>::iterator> m_movementAnimation;
-	sf::Clock m_clock;
-	std::shared_ptr<Hitbox> m_hitbox;
-	Inventory m_inventory;
-	bool m_defined;
-	bool m_moving;
-	bool m_cameraFollowing;
-	unsigned short m_x;
-	unsigned short m_y;
-	short m_speedX;
-	short m_speedY;
-	short m_width;
-	short m_height;
-	short m_maxX;
-	short m_maxY;
-};
-
+	/**
+	* \brief Modify the width of the team	.
+	*
+	* \param w The new value.
+	* \retu
