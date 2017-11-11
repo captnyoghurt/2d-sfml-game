5c848b948e0d02efa047b2d250421bc0fbe9912d
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:03:00 +0100

    Comment (Mana.cpp)

--- ConsoleApplication1/Battle/Status/Mana.cpp
+++ ConsoleApplication1/Battle/Status/Mana.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Mana of a fighter.
+*
+*/
+
 #include "../../constants.h"
 #include "Mana.h"
 #include "Spell.h"
