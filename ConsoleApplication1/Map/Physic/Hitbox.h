fd7780ff958ad1a537927bc5f91485ae46498b0e
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 18:00:59 +0100

    Comment (Hitbox.h)

--- ConsoleApplication1/Map/Physic/Hitbox.h
+++ ConsoleApplication1/Map/Physic/Hitbox.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  A physic hitbox, to see if there is a collision.
+*
+*/
+
 #pragma once
 
 #include <SFML/Graphics.hpp>
@@ -13,31 +27,85 @@ public:
 
 	Hitbox& operator=(const Hitbox &hb);
 public:
-	// Return the id of the Hitbox
+	/**
+	* \brief Return the id of the Hitbox	.
+	*
+	* \return int object.
+	*/
 	int getId() const;
-	// Return the abciss of the Hitbox
+	/**
+	* \brief Return the abciss of the Hitbox	.
+	*
+	* \return int object.
+	*/
 	int getX() const;
-	// Return the y position of the Hitbox
+	/**
+	* \brief Return the y position of the Hitbox	.
+	*
+	* \return int object.
+	*/
 	int getY() const;
-	// Return the width of the Hitbox
+	/**
+	* \brief Return the width of the Hitbox	.
+	*
+	* \return int object.
+	*/
 	int getWidth() const;
-	// Return the height of the Hitbox
+	/**
+	* \brief Return the height of the Hitbox	.
+	*
+	* \return int object.
+	*/
 	int getHeight() const;
 
-	// Modify the id of the Hitbox
+	/**
+	* \brief Modify the id of the Hitbox	.
+	*
+	* \param id The new value.
+	* \return int object.
+	*/
 	int setId(const int &id);
-	// Modify the abciss of the Hitbox
+	/**
+	* \brief Modify the abciss of the Hitbox	.
+	*
+	* \param x The new value.
+	* \return int object.
+	*/
 	int setX(const int &x);
-	// Modify the y position of the Hitbox
+	/**
+	* \brief Modify the y position of the Hitbox	.
+	*
+	* \param y The new value.
+	* \return int object.
+	*/
 	int setY(const int &y);
-	// Modify the width of the Hitbox
+	/**
+	* \brief Modify the width of the Hitbox	.
+	*
+	* \param w The new value.
+	* \return int object.
+	*/
 	int setWidth(const int &w);
-	// Modify the height of the Hitbox
+	/**
+	* \brief Modify the height of the Hitbox	.
+	*
+	* \param h The new value.
+	* \return int object.
+	*/
 	int setHeight(const int &h);
 
-	// Modify x & y of the Hitbox
+	/**
+	* \brief Modify x & y of the Hitbox	.
+	*
+	* \param dy The new value.
+	* \return int object.
+	*/
 	int move(const int &dx, const int &dy);
-	// Move to a position
+	/**
+	* \brief Move to a position	.
+	*
+	* \return int object.
+	*/
 	int moveTo(const int &x, const int &y);
 
 public:
@@ -47,4 +115,3 @@ public:
 	int m_width;
 	int m_height;
 };
-
