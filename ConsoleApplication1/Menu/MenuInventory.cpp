eb535e0d4f49cf7f7b1be13fa4d8d60c06939dd2
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:20:49 +0100

    Comment (MenuInventory.cpp)

--- ConsoleApplication1/Menu/MenuInventory.cpp
+++ ConsoleApplication1/Menu/MenuInventory.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Menu to show the bag.
+*
+*/
+
 #include <memory>
 #include <algorithm>
 #include "MenuInventory.h"
