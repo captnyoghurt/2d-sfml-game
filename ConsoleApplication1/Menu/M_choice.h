3a5a1c055d342111ae1560365df4501f146a1487
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:21:14 +0100

    Comment (M_choice.h)

--- ConsoleApplication1/Menu/M_choice.h
+++ ConsoleApplication1/Menu/M_choice.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  A choice in the menu choice.
+*
+*/
+
 #pragma once
 
 #include <list>
@@ -16,40 +30,109 @@ public:
 
 	M_choice& operator=(const M_choice &ch);
 public:
-	// Return if the choice has been initialized
+	/**
+	* \brief Return if the choice has been initialized	.
+	*
+	* \return bool object.
+	*/
 	bool getInitialized() const;
-	// Return if the choice should be shown
+	/**
+	* \brief Return if the choice should be shown	.
+	*
+	* \return bool object.
+	*/
 	bool getShown() const;
-	// Return if the choice is enabled
+	/**
+	* \brief Return if the choice is enabled	.
+	*
+	* \return bool object.
+	*/
 	bool getEnabled() const;
-	// Return the surface of the choice
+	/**
+	* \brief Return the surface of the choice	.
+	*
+	* \return std::list<std::pair<ManageSurfaces::e_thing, object.
+	*/
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator getSurface();
-	// Return the action made by the choice
+	/**
+	* \brief Return the action made by the choice	.
+	*
+	* \return IG_Action object.
+	*/
 	IG_Action getAction() const;
-	// Return the position X of the choice on the screen
+	/**
+	* \brief Return the position X of the choice on the screen	.
+	*
+	* \return int object.
+	*/
 	int getX() const;
-	// Return the position Y of the choice on the screen
+	/**
+	* \brief Return the position Y of the choice on the screen	.
+	*
+	* \return int object.
+	*/
 	int getY() const;
 
-	// Return the surface of the choice with modifying possibilities
+	/**
+	* \brief Return the surface of the choice with modifying possibilities	.
+	*
+	* \return std::list<std::pair<ManageSurfaces::e_thing, object.
+	*/
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator& getRealSurface();
-	// Return the render texture manager with modifying possibilities
+	/**
+	* \brief Return the render texture manager with modifying possibilities	.
+	*
+	* \return ManageRenderTexture& object.
+	*/
 	ManageRenderTexture& getRealRenderTextureManager();
 
-	// Modify if the choice should be shown
+	/**
+	* \brief Modify if the choice should be shown	.
+	*
+	* \param dy The new value.
+	* \return int object.
+	*/
 	int setShown(const bool &b, const int &dx, const int &dy);
-	// Modify if the choice should be enabled
+	/**
+	* \brief Modify if the choice should be enabled	.
+	*
+	* \param b The new value.
+	* \return int object.
+	*/
 	int setEnabled(const bool &b);
-	// Modify the action made by the choice
+	/**
+	* \brief Modify the action made by the choice	.
+	*
+	* \param act The new value.
+	* \return int object.
+	*/
 	int setAction(IG_Action &act);
-	// Modify the position X of the choice on the screen
+	/**
+	* \brief Modify the position X of the choice on the screen	.
+	*
+	* \param x The new value.
+	* \return int object.
+	*/
 	int setX(const int &x);
-	// Modify the position Y of the choice on the screen
+	/**
+	* \brief Modify the position Y of the choice on the screen	.
+	*
+	* \param y The new value.
+	* \return int object.
+	*/
 	int setY(const int &y);
 
-	// Load the choice
+	/**
+	* \brief Load the choice	.
+	*
+	* \return int object.
+	*/
 	int load(ManageSurfaces& surf, const std::string &str, sf::Font &f, const int &x, const int &y, const bool &sh, IG_Action &, const int &w = 0, const int &h = 0);
-	// Clear the surfaces of the choice
+	/**
+	* \brief Clear the surfaces of the choice	.
+	*
+	* \return int object.
+	*/
 	int clear(ManageSurfaces &surf);
 private:
 	bool m_initialized;
@@ -61,4 +144,3 @@ private:
 	int m_y;
 
 };
-
