69f23517ba8cb7f49d37197a3ca5a93a17ba50a2
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:03:50 +0100

    Comment (Spell.cpp)

--- ConsoleApplication1/Battle/Status/Spell.cpp
+++ ConsoleApplication1/Battle/Status/Spell.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  A spell of a fighter. Can be cast.
+*
+*/
+
 #include "Spell.h"
 #include "../../Error/ValueException.h"
 
