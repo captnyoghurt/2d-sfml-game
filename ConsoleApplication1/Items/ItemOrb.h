af7fe57777bd0521707c8a86d257bc3ec6200fd4
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 18:00:31 +0100

    Comment (ItemOrb.h)

--- ConsoleApplication1/Items/ItemOrb.h
+++ ConsoleApplication1/Items/ItemOrb.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  An orb, which can give a bonus.
+*
+*/
+
 #pragma once
 
 #include "Item.h"
@@ -11,4 +25,3 @@ public:
 	explicit ItemOrb(const Item &item);
 	~ItemOrb();
 };
-
