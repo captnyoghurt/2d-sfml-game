897fb3237217ffc407dd89300c0f62ce6800d490
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 16:45:17 +0100

    Comment (B_EventAttack.cpp)

--- ConsoleApplication1/Battle/B_Event/B_EventAttack.cpp
+++ ConsoleApplication1/Battle/B_Event/B_EventAttack.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Make an attack.
+*
+*/
+
 #include "B_EventAttack.h"
 #include "../../constants.h"
 #include "../../Battle/Battle.h"
