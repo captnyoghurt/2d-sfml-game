f647d0e5f703a49a39380a40cbe1f5ecba80adb4
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:54:36 +0100

    Comment (B_EventDefense.h)

--- ConsoleApplication1/Battle/B_Event/B_EventDefense.h
+++ ConsoleApplication1/Battle/B_Event/B_EventDefense.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Make a battle defense.
+*
+*/
+
 #pragma once
 
 #include "B_Event.h"
@@ -10,15 +24,27 @@ public:
 	~B_EventDefense();
 
 public:
-	// Return if the fighter destination of the defense
+	/**
+	* \brief Return if the fighter destination of the defense	.
+	*
+	* \return std::shared_ptr<Fighter> object.
+	*/
 	std::shared_ptr<Fighter> getDestination();
 
-	// Modify if the fighter destination of the defense
+	/**
+	* \brief Modify if the fighter destination of the defense	.
+	*
+	* \param dest The new value.
+	* \return int object.
+	*/
 	int setDestination(std::shared_ptr<Fighter> dest);
 
-	// Do the event
+	/**
+	* \brief Do the event	.
+	*
+	* \return virtual object.
+	*/
 	virtual int execute(Battle *bat, ManageSound& snd);
 private:
 	std::shared_ptr<Fighter> m_destination;
 };
-
