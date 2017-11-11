ddb2e8b5cebb5dc3495709088a9df0509367a7c5
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 18:02:23 +0100

    Comment (SurfaceText.h)

--- ConsoleApplication1/Map/SurfaceText.h
+++ ConsoleApplication1/Map/SurfaceText.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Text type of surface.
+*
+*/
+
 #pragma once
 
 #include <SFML\Graphics.hpp>
@@ -13,4 +27,3 @@ public:
 	explicit SurfaceText(Text t);
 	~SurfaceText();
 };
-
