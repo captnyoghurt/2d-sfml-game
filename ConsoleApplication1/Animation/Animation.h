deb2c3a2c354dac3ffd2f391d35c2b791a06bddf
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 16:40:08 +0100

    Comment (Animation.h)

--- ConsoleApplication1/Animation/Animation.h
+++ ConsoleApplication1/Animation/Animation.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  The class is an animation to display.
+*
+*/
+
 #pragma once
 
 #include <SFML\Graphics.hpp>
@@ -13,40 +27,105 @@ public:
 	~Animation();
 
 public:
-	// Return if the animation moves
+	/**
+	* \brief Return if the animation moves	.
+	*
+	* \return bool object.
+	*/
 	bool getMoving() const;
-	// Return if the animation is finished
+	/**
+	* \brief Return if the animation is finished	.
+	*
+	* \return bool object.
+	*/
 	bool getFinished() const;
-	// Return if the animation is supposed to be killed when its done
+	/**
+	* \brief Return if the animation is supposed to be killed when its done	.
+	*
+	* \return bool object.
+	*/
 	bool getKillWhenDone() const;
-	// Return the remaining number of loop
+	/**
+	* \brief Return the remaining number of loop	.
+	*
+	* \return int object.
+	*/
 	int getNbLoop() const;
-	// Return the animation refresh time
+	/**
+	* \brief Return the animation refresh time	.
+	*
+	* \return int object.
+	*/
 	int getRefreshTime() const;
-	// Return the distance walked by the animation at each call
+	/**
+	* \brief Return the distance walked by the animation at each call	.
+	*
+	* \return int object.
+	*/
 	int getManathanDistance() const;
-	// Return the destination position X
+	/**
+	* \brief Return the destination position X	.
+	*
+	* \return int object.
+	*/
 	int getEndX() const;
-	// Return the destination position Y
+	/**
+	* \brief Return the destination position Y	.
+	*
+	* \return int object.
+	*/
 	int getEndY() const;
-	// Return the location of the sprites rect
+	/**
+	* \brief Return the location of the sprites rect	.
+	*
+	* \return std::vector<sf::Rect<short>> object.
+	*/
 	std::vector<sf::Rect<short>> getRectSprites() const;
 
-	// Modify the animation refresh time
+	/**
+	* \brief Modify the animation refresh time	.
+	*
+	* \param t The new value.
+	* \return int object.
+	*/
 	int setRefreshTime(const int &t);
 
-	// Return the surface used
+	/**
+	* \brief Return the surface used	.
+	*
+	* \return std::list<std::pair<ManageSurfaces::e_thing, object.
+	*/
 	std::list<std::pair<ManageSurfaces::e_thing, std::shared_ptr<Surface>>>::iterator getRealSurface();
 
-	// Stop instantly the animation
+	/**
+	* \brief Stop instantly the animation	.
+	*
+	* \return int object.
+	*/
 	int stop();
-	// Stop after the end of the animation
+	/**
+	* \brief Stop after the end of the animation	.
+	*
+	* \return int object.
+	*/
 	int stopAfter();
-	// Kill instantly the animation
+	/**
+	* \brief Kill instantly the animation	.
+	*
+	* \return int object.
+	*/
 	int kill();
-	// Update the animation
+	/**
+	* \brief Update the animation	.
+	*
+	* \return int object.
+	*/
 	int update();
-	// Start the animation
+	/**
+	* \brief Start the animation	.
+	*
+	* \return int object.
+	*/
 	int start(int endX, int endY, int manDist, int loop = -1, bool killWhenDone = false);
 	int start(int loop = -1, bool killWhenDone = false);
 
@@ -64,4 +143,3 @@ private:
 	std::vector<sf::Rect<short>> m_rectSprites;
 	int m_rectAct;
 };
-
