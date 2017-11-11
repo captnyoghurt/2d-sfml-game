6a70cab5578bfef64944d831adcf8c3d4bed8841
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:59:57 +0100

    Comment (ManageEvent.h)

--- ConsoleApplication1/Event/ManageEvent.h
+++ ConsoleApplication1/Event/ManageEvent.h
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  Call the actions according to the trigger.
+*
+*/
+
 #pragma once
 
 #include <vector>
@@ -11,29 +25,62 @@ public:
 	~ManageEvent();
 
 public:
-	// Return the actual key event layer
+	/**
+	* \brief Return the actual key event layer	.
+	*
+	* \return int object.
+	*/
 	int getKeyEventLayer() const;
-	// Return the key event list
+	/**
+	* \brief Return the key event list	.
+	*
+	* \return std::vector< object.
+	*/
 	std::vector< std::vector< std::pair<unsigned short, unsigned short> > > getKeyEventList() const;
-	// Retuen the maching key event list
+	/**
+	* \brief Retuen the maching key event list	.
+	*
+	* \return std::vector<IG_KeyEvent> object.
+	*/
 	std::vector<IG_KeyEvent> getMatchingKeyEvent() const;
 
-	// Modify the actual key event layer
+	/**
+	* \brief Modify the actual key event layer	.
+	*
+	* \param l The new value.
+	* \return int object.
+	*/
 	int setKeyEventLayer(const int &l);
-	// Modify a key event
+	/**
+	* \brief Modify a key event	.
+	*
+	* \param true The new value.
+	* \return int object.
+	*/
 	int setKeyEvent(int layer, int code, unsigned short matchCode, bool down = true);
 	
-	// Create a matching
+	/**
+	* \brief Create a matching	.
+	*
+	* \return unsigned object.
+	*/
 	unsigned short createMatchingKey(const IG_KeyEvent &k);
 	unsigned short createMatchingKey(IG_Action a);
-	// Create the layers
+	/**
+	* \brief Create the layers	.
+	*
+	* \return int object.
+	*/
 	int createLayers(const int &l);
 	
-	// Update event
+	/**
+	* \brief Update event	.
+	*
+	* \return int object.
+	*/
 	int updateEvent(Game &g);
 private:
 	int m_keyEventLayer;
 	std::vector< std::vector< std::pair<unsigned short, unsigned short> > > m_keyEventList;
 	std::vector<IG_KeyEvent> m_matchingKeyEvent;
 };
-
