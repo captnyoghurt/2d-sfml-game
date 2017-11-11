bc26a4ab658d98027cb9f61c44469bf1b31d6a7a
Paul Coignet <crushing.of.files@gmail.com>
2017-11-11 17:06:28 +0100

    Comment (IG_Action.cpp)

--- ConsoleApplication1/Event/IG_Action.cpp
+++ ConsoleApplication1/Event/IG_Action.cpp
@@ -1,3 +1,17 @@
+/**
+*  @file
+*  @author  Paul Coignet
+*  @date    11 / 11 / 2017
+*  @version 0.1
+*
+*  @brief Class header.
+*
+*  @section DESCRIPTION
+*
+*  An action to play after a trigger.
+*
+*/
+
 #include "IG_Action.h"
 
 
